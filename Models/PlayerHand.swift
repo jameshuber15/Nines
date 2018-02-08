839a6af589ebeee4353dc0a1983b2a0c578bf333
James Huber <james.huber@dailymotion.com>
2018-02-08 10:10:41 -0500

    Adding CardGroup object to signify a group of cards (Deck,PlayerHand)

--- Models/PlayerHand.swift
+++ Models/PlayerHand.swift
@@ -8,43 +8,24 @@
 
 import Foundation
 
-class PlayerHand {
+class PlayerHand : CardGroup {
     
-    internal var hand : [PlayingCard]
-    
-    init(hand : [PlayingCard]) {
-        self.hand = hand
+    override init(cards : [PlayingCard]) {
+        super.init(cards: cards)
     }
     
     func mergeDiscardPile(discardPile: [PlayingCard]) {
-        self.hand.append(contentsOf: discardPile)
+        self.cards.append(contentsOf: discardPile)
         self.sortHandByRank()
-    }
-    
-    func sortHandByRank() {
-        guard self.hand.count > 1 else { return }
-        
-        for x in 0 ..< self.hand.count - 1 {
-            var lowest = x
-            for y in x + 1 ..< self.hand.count {
-                if self.hand[y].rank < self.hand[lowest].rank {
-                    lowest = y
-                }
-            }
-    
-            if x != lowest {
-                self.hand.swapAt(x, lowest)
-            }
-        }
     }
     
     func findValidCards(topCard: PlayingCard) -> [PlayingCard] {
         var playableCards : [PlayingCard] = []
-        for card in 0 ..< self.hand.count {
-            if topCard.getRank() <= hand[card].getRank() {
-                playableCards.append(hand[card])
-            } else if hand[card].getRank() == 10 || hand[card].getRank() == 2 {
-                playableCards.append(hand[card])
+        for card in 0 ..< self.cards.count {
+            if topCard.getRank() <= cards[card].getRank() {
+                playableCards.append(cards[card])
+            } else if cards[card].getRank() == 10 || cards[card].getRank() == 2 {
+                playableCards.append(cards[card])
             }
         }
         return playableCards
